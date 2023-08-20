b0b6e9b4374e36714068b25d740b429e4a73be1e
NishantAmera <nishant220199@gmail.com>
2023-08-21 00:58:41 +0530

    added app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
-console.log('Hello, Node.js Application!');
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, world!\n');
+});
+
+const port = 8080;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
