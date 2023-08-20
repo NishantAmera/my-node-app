7a39a30695d4832c59436c74f82dc57b9a5076aa
NishantAmera <nishant220199@gmail.com>
2023-08-21 01:54:48 +0530

    updated template

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hi everyone!\n'); // Updated message
+  res.end('Hello, world!\n'); // Updated message
 });
 
 const port = 8080;
